61027fa1558255b7f63c2befb578b3f784f00432
Sean Wong <seanwong24@live.com>
2021-12-19 16:47:27 -0600

    allow applying styles

--- src/components/awesome-markdown/awesome-markdown.tsx
+++ src/components/awesome-markdown/awesome-markdown.tsx
@@ -1,5 +1,6 @@
 import { Component, Host, h, ComponentInterface, Prop, Element } from '@stencil/core';
 import { marked } from 'marked';
+import { observeMutation } from '@awesome-elements/utils';
 
 @Component({
   tag: 'awesome-markdown',
@@ -14,15 +15,33 @@ export class AwesomeMarkdown implements ComponentInterface {
    */
   @Prop() markdown: string;
 
+  componentDidLoad() {
+    observeMutation.call(this, this.hostElement, [this.updateStyle], {
+      childList: true,
+      subtree: true,
+    });
+    this.updateStyle();
+  }
+
   componentDidRender() {
-    this.hostElement.shadowRoot.innerHTML = marked.parse(this.markdown || '');
+    this.hostElement.shadowRoot.querySelector('body').innerHTML = marked.parse(this.markdown || '');
   }
 
   render() {
     return (
       <Host>
-        <slot></slot>
+        <html>
+          <head></head>
+          <body></body>
+        </html>
       </Host>
     );
+  }
+
+  private updateStyle() {
+    const templateElement = this.hostElement.querySelector('template');
+    const templateContent = templateElement?.content.cloneNode(true);
+    this.hostElement.shadowRoot.querySelector('head').innerHTML = '';
+    this.hostElement.shadowRoot.querySelector('head').append(templateContent);
   }
 }
